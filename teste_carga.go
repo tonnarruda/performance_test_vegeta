78154169b78d772a57f19a4e423f63cb59bc8f4f
ton-arruda <georgem.isaac.com.br>
2023-04-18 14:45:15 -0300

    Ajuste

--- teste_carga.go
+++ teste_carga.go
@@ -10,11 +10,11 @@ import (
 
 func main() {
 	startTime := time.Now()
-	rate := vegeta.Rate{Freq: 50, Per: time.Second} // taxa de solicitações por segundo
-	duration := 10 * time.Second                    // duração do teste
+	rate := vegeta.Rate{Freq: 100, Per: time.Second} // taxa de solicitações por segundo
+	duration := 15 * time.Second                     // duração do teste
 
-	// Criar um Targeter estático com a URL alvo
-	targeter := vegeta.NewStaticTargeter(vegeta.Target{
+	// Criar um target estático com a URL alvo
+	target := vegeta.NewStaticTargeter(vegeta.Target{
 		Method: "GET",
 		URL:    "https://restful-booker.herokuapp.com/booking",
 	})
@@ -24,12 +24,12 @@ func main() {
 
 	// Realizar o ataque e coletar as métricas
 	var metrics vegeta.Metrics
-	for res := range attacker.Attack(targeter, rate, duration, "PerformanceTest") {
+	for res := range attacker.Attack(target, rate, duration, "PerformanceTest") {
 		metrics.Add(res)
 
 		// Verificar se há erro na resposta e logar apenas se houver
 		if res.Error != "" {
-			log.Printf("Response: %s, Code: %d, Error: %v\n", res.Timestamp, res.Code, res.Error)
+			log.Printf("Resposta: %s, Código: %d, Erro: %v\n", res.Timestamp, res.Code, res.Error)
 		} else {
 			log.Printf("StatusCode: %d\n", res.Code)
 		}
@@ -39,11 +39,11 @@ func main() {
 	fmt.Printf("Tempo total de execução: %s\n", elapsedTime)
 
 	// Exibir as métricas
-	fmt.Printf("Requests: %d\n", metrics.Requests)
-	fmt.Printf("Success: %0.2f%%\n", metrics.Success*100)
-	fmt.Printf("Latency mean: %s\n", metrics.Latencies.Mean)
-	fmt.Printf("Latency 50th percentile: %s\n", metrics.Latencies.P50)
-	fmt.Printf("Latency 90th percentile: %s\n", metrics.Latencies.P90)
+	fmt.Printf("Requisições: %d\n", metrics.Requests)
+	fmt.Printf("Sucesso: %.2f%%\n", metrics.Success*100)
+	fmt.Printf("Média de latência: %s\n", metrics.Latencies.Mean)
+	fmt.Printf("Percentil 50 de latência: %s\n", metrics.Latencies.P50)
+	fmt.Printf("Percentil 90 de latência: %s\n", metrics.Latencies.P90)
 	fmt.Printf("Valor médio de bytes de entrada registrados: %.2f\n", metrics.BytesIn.Mean)
 	fmt.Printf("Tempo total de execução: %.2f segundos\n", elapsedTime.Seconds())
 }
